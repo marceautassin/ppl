ae699b317dfc5bb4926253dd6e1d30f9562754ae
Marceau Tassin <marceau.tassin@gmail.com>
2020-02-26 16:19:01 +0100

    model: ajout du accepts_nested_attributes_for et du invers_of

--- app/models/doc_line.rb
+++ app/models/doc_line.rb
@@ -1,7 +1,7 @@
 class DocLine < ApplicationRecord
   include Enumerable
 
-  belongs_to :document
+  belongs_to :document, inverse_of: :doc_lines
 
   enum category: [ :salaire_brut, :salaire_net_paye, :impot_revenu, :conge_n_1, :conge_n, :rtt]
 
--- app/models/document.rb
+++ app/models/document.rb
@@ -1,5 +1,9 @@
 class Document < ApplicationRecord
   belongs_to :user
-  has_many :doc_lines
+  has_many :doc_lines, inverse_of: :document
   has_one_attached :photo
+
+  accepts_nested_attributes_for :doc_lines,
+                                reject_if: proc { |attributes| attributes[:name].blank? },
+                                allow_destroy: true
 end
