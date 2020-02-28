5a40f2ff3481ba0fb8e932bbdcc48764de1a2125
Marceau Tassin <marceau.tassin@gmail.com>
2020-02-28 16:17:43 +0100

    fix bug on sign out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ class ApplicationController < ActionController::Base
     documents_path
   end
 
-  def after_sign_out_path_for(resource_or_scope)
+  def after_sign_out_path_for(resource)
     root_path
   end
 end
