222de1217cdff69144d10a7e8b11d385ae3d9a63
quentin de malherbe <quentdemalherbe@gmail.com>
2020-02-29 15:17:11 +0100

    avatar plus qq validations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,10 @@ class ApplicationController < ActionController::Base
 
     def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :social_security_number ])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :social_security_number, :photo])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :social_security_number ])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :social_security_number, :photo])
   end
 
   def after_sign_in_path_for(resource)
@@ -18,7 +18,7 @@ class ApplicationController < ActionController::Base
     documents_path
   end
 
-  def after_sign_out_path_for(resource_or_scope)
+  def after_sign_out_path_for(resource)
     root_path
   end
 end
