fd7e7e9364a513c4939ac3f32e0304ac96d08fdf
benoit castillon <benoit.castillon@gmail.com>
2020-03-04 11:32:21 +0100

    step1

--- app/controllers/simulations_controller.rb
+++ app/controllers/simulations_controller.rb
@@ -3,21 +3,22 @@ class SimulationsController < ApplicationController
   def index
 
     #REVENU BRUT TOTAL DES 12 DERNIERS MOIS
-    @somme = 0
+    somme = 0
     Document.last(12).each {|document|
-      @somme += document.doc_lines.where(category: :salaire_brut).last.amount
+      somme += document.doc_lines.where(category: :salaire_brut).last.amount
     }
-    @revenutotallastyear = @somme.round(2).to_s.reverse.gsub(/...(?=.)/,'\& ').reverse + ' €'
+    @revenutotallastyear = somme.round(2).to_s.reverse.gsub(/...(?=.)/,'\& ').reverse + ' €'
 
 
     #SALAIRE JOURNALIER DE REFERENCE
-    @sjr_integer = @somme/(261*1.4)
+    @sjr_integer = somme/(261*1.4)
     @sjr = @sjr_integer.round(2).to_s.reverse.gsub(/...(?=.)/,'\& ').reverse + ' €'
 
 
     #ALLOCATION JOURNALIERE DE RETOUR A L'EMPLOI
     @arej_m1 =  @sjr_integer * 0.404 + 12
     @arej_m2 = @sjr_integer * 0.57
+
 
     if @arej_m1 > @arej_m2
       @are_j_m1 =@arej_m1.round(2).to_s.reverse.gsub(/...(?=.)/,'\& ').reverse + ' €'
@@ -36,13 +37,15 @@ class SimulationsController < ApplicationController
     @arej_m2_postdecote_plancher = '84.33 €'
 
     if @arej_m2_postdecote > 84.33
-
     else
       @arej_m2_postdecote_plancher = '84.33 €'
     end
 
-     # DUREE D'INDEMNISATION
+    # AGE DEPART RETRAITE AU PLUS TOT
+    @age_tot = '62 ans'
 
+    # AGE DEPART RETRAITE AU PLUS TARD
+    @age_tot = ' ans'
 
 
 
